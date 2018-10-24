351d6ad46ffb580de338ce59cf5fa6bec6934f4e
Caleb Christensen <Caleb.Christ@gmail.com>
2018-10-24 16:18:33 -0400

    styling update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
-import { AboutComponent } from './about/about.component'
-import { HomePageComponent } from '/home-page/home-page.component'
+// import { AboutComponent } from './about/about.component'
+import { HomePageComponent } from './home-page/home-page.component'
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomePageComponent},
-  {path: 'about', component: AboutComponent},
+  // {path: 'about', component: AboutComponent},
   {path: 'products', component: ProductsComponent}
 
 ]
