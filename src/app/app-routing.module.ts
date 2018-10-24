f6983cd6e52ce66f91196d893dced91a38ea9f02
Justin Weiskittel <justinweiskittel@gmail.com>
2018-10-24 15:48:40 -0400

    products updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component'
+import { HomePageComponent } from '/home-page/home-page.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomePageComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'products', component: ProductsComponent}
+
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
